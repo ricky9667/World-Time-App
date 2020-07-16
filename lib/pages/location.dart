7cf5a96051031c927869785c53028a1ef88d2251
ricky9667 <ricky9667@gmail.com>
2020-07-16 23:46:24 +0800

    Implement getTime()

---
+++ lib/pages/location.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build function ran');
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[600],
+        title: Text('Choose a location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: RaisedButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Text('counter: $counter'),
+      ),
+    );
+  }
+
+  Widget buildPasswordText() {
+    return Text(
+      ""
+    );
+  }
+}
